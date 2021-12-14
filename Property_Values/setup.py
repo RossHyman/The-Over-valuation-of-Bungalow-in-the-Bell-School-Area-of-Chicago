72cccc711b2164671c625e1b7cc2a1dff817dffd
Ross Hyman <rosshyman@att.net>
2021-12-14 11:28:25 -0600

    first commit

---
+++ Property_Values/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='exploring reduction of bias of property assesments',
+    author='Ross Hyman',
+    license='MIT',
+)
